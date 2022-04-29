dc68d939f1d985f79a616f50af9432beed7c384f
alabador <austinlabador@gmail.com>
2022-04-29 02:23:42 -0700

    Discard slider logic, got stuck on divs and grids replicating itself

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const container = document.querySelector('.container');
 const slider = document.querySelector('.slider');
+const button = document.querySelector('.button-prompt');
 
 for (let i=0; i<(16*16); i++) {
     let div = document.createElement('div');
     div.className = 'square';
-    // div.innerText = i;
     container.appendChild(div);
 }
+
+button.addEventListener('click', function() {
+    let columns = prompt("How many columns?", "16");
+    let rows = prompt("How many rows?", "16");
+    
+})
 
 const squares = document.querySelectorAll('.square');
 
