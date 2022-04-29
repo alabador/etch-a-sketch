9f2fb478c0bfe8dbe0386e9bd1fe53ef086a527a
alabador <austinlabador@gmail.com>
2022-04-29 11:26:30 -0700

    Remove button functions, update innerText based on slider value

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
 const slider = document.querySelector('.slider');
-const button = document.querySelector('.button-prompt');
+const gridSizeText = document.querySelector('.grid-size-text');
 let gridSize = 0;
 
 for (let i=0; i<(16*16); i++) {
@@ -17,29 +17,10 @@ container.addEventListener('mouseover', function(e) {
     }
 })
 
-// button.addEventListener('click', function() {
-//     gridSize = prompt("Choose a grid size", "16");
-    
-//     const squares = document.querySelectorAll('.square');
-//     //I think I can also use remove child here
-//     squares.forEach(square => {
-//         square.remove();
-//     });
-    
-//     container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-//     container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-
-//     for (let i=0; i<(gridSize*gridSize); i++) {
-//         let div = document.createElement('div');
-//         div.className = 'square';
-//         container.appendChild(div);
-//     }
-    
-
-// })
 
 slider.addEventListener('input', function() {
     gridSize = slider.value;
+    gridSizeText.innerText = `${gridSize} x ${gridSize}`;
     
     const squares = document.querySelectorAll('.square');
     //I think I can also use remove child here
