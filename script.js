9fc5e3a72f41063c6ca2d36e9b5c8f1e82e99314
alabador <austinlabador@gmail.com>
2022-04-29 10:53:09 -0700

    Change grid parameters to squares/equal length & width only

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const container = document.querySelector('.container');
 const slider = document.querySelector('.slider');
 const button = document.querySelector('.button-prompt');
-let columns = 0;
-let rows = 0;
+let gridSize = 0;
+// let columns = 0;
+// let rows = 0;
 
 for (let i=0; i<(16*16); i++) {
     let div = document.createElement('div');
@@ -19,8 +20,9 @@ container.addEventListener('mouseover', function(e) {
 })
 
 button.addEventListener('click', function() {
-    columns = prompt("How many columns?", "16");
-    rows = prompt("How many rows?", "16");
+    gridSize = prompt("Choose a grid size", "16");
+    // columns = prompt("How many columns?", "16");
+    // rows = prompt("How many rows?", "16");
     
     const squares = document.querySelectorAll('.square');
     //I think I can also use remove child here
@@ -28,10 +30,10 @@ button.addEventListener('click', function() {
         square.remove();
     });
     
-    container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
-    for (let i=0; i<(columns*rows); i++) {
+    for (let i=0; i<(gridSize*gridSize); i++) {
         let div = document.createElement('div');
         div.className = 'square';
         container.appendChild(div);
