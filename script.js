e1b2c745db59b4934b328b4e4e8c6d5123829562
alabador <austinlabador@gmail.com>
2022-05-02 11:20:53 -0700

    Change color selection to use datasets rather than classes

--- script.js
+++ script.js
@@ -1,47 +1,68 @@
+/*Variables*/
 const container = document.querySelector('.container');
 const slider = document.querySelector('.slider');
 const gridSizeText = document.querySelector('.grid-size-text');
 const resetButton = document.querySelector('.reset-button');
 const colors = document.querySelectorAll('.color');
-let gridSize = 0;
-let colorArr = [];
 
+let color;
+let gridSize = 0;
+
+/*Create initial grid*/
 for (let i=0; i<(16*16); i++) {
     let div = document.createElement('div');
     div.className = 'square';
     container.appendChild(div);
 }
 
-// colors.forEach(color => {
-//     color.addEventListener('click', function(e) {
-//         let fullClass = e.target.className;
-//         let colorClass = fullClass.replace('color', '');
-//         return colorClass;
-//     })
-// });
+/*Click on color buttons to choose color*/
+colors.forEach(currentColor => {
+    currentColor.addEventListener('click', chooseColor);
+});
 
+/*Color selection*/
+function chooseColor(e) {
+    if (e.target.dataset.color == 'red') {
+        color = 'red';
+    }
+    if (e.target.dataset.color == 'orange') {
+        color = 'orange'; 
+    }
+    if (e.target.dataset.color == 'yellow') {
+        color = 'yellow';
+    }
+    if (e.target.dataset.color == 'green') {
+        color = 'green';
+    }
+    if (e.target.dataset.color == 'blue') {
+        color = 'blue';
+    }
+    if (e.target.dataset.color == 'violet') {
+        color = 'violet';
+    }
+    if (e.target.dataset.color == 'white') {
+        color = 'white';
+    }
+    if (e.target.dataset.color == 'black') {
+        color = 'black';
+    }
+}
 
-
-// colors.forEach(color => {
-//     colorArr.push(color.className.replace('color ', ''));
-//     console.log(colorArr);
-// });
-
+/*Changes pixel color on hover*/
 container.addEventListener('mouseover', function(e) {
     const target = e.target;
     if (target.matches('.square')) {
-        target.classList.add('change-color');
-        // target.classList.add(colorClass);
+        target.style.backgroundColor = color;
     }
 })
 
-
+/*Creates new grid based on slider input*/
 slider.addEventListener('input', function() {
     gridSize = slider.value;
     gridSizeText.innerText = `${gridSize} x ${gridSize}`;
     
     const squares = document.querySelectorAll('.square');
-    //I think I can also use remove child here
+    
     squares.forEach(square => {
         square.remove();
     });
@@ -58,6 +79,7 @@ slider.addEventListener('input', function() {
 
 })
 
+/*Reset etch-a-sketch*/
 resetButton.addEventListener('click', function() {
     const squares = document.querySelectorAll('.square');
     squares.forEach(square => {
